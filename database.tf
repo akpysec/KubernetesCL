ab503ad8476c866d7f53d14b3df925fefc08df1f
Andrey Kolihanov <akpysec@gmail.com>
2021-12-21 12:27:16 +0200

    rm sg

--- database.tf
+++ database.tf
@@ -22,28 +22,3 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "db-subnet-group"
   subnet_ids = aws_subnet.rds_subnet_.*.id
 }
-
-resource "aws_security_group" "db_sg" {
-  name        = "to_DB"
-  description = "Allow inbound traffic from EKS"
-  vpc_id      = aws_vpc.sunny_vpc.id
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.1.0/0", "10.0.2.0/24"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name  = var.sg_tags[0]
-    Owner = var.sg_tags[1]
-  }
-}
