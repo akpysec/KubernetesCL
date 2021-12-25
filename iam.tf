2871bc5503532510fefd45d5b27d700e12594797
EC2 Default User <ec2-user@ip-10-0-0-163.ec2.internal>
2021-12-25 12:55:44 +0000

    refactoring

--- iam.tf
+++ iam.tf
@@ -38,3 +38,4 @@ resource "aws_iam_role_policy_attachment" "sunny-cluster-AmazonEKSVPCResourceCon
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.sunny-cluster.name
 }
+
